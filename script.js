0bdb0bf88c7822ae407942bbaa165ec626c91b7b
Aanushkaaa <91602302+Aanushkaaa@users.noreply.github.com>
2022-05-01 16:38:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ window.onload = () => renderTranscationList();
 let addtranscation = document.querySelector(".btn");
 addtranscation.addEventListener("click", transcationlist);
 
-
 function transcationlist() {
     let transcationname = document.querySelector("#text").value;
     let transcationamount = Number.parseFloat(
@@ -84,17 +83,15 @@ let createlist = (transcationname, transcationamount, positive) => {
     let liclass = positive ? "plus" : "minus";
     let symbol = positive ? "+ " : "";
 
-    
     //label tag
     let label = document.createElement("label");
     label.innerHTML = transcationname;
 
-
     //spantag
+
     let span = document.createElement("span");
     span.setAttribute("class", "money " + liclass);
     span.innerHTML = symbol + transcationamount;
-
 
     // li tag
     let li = document.createElement("li");
